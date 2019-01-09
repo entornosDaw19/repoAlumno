ebd4ae4f3744606e2b29fe80e06a4cee0d215501
entornosDaw19 <ciclos201819@gmail.com>
2019-01-09 20:25:09 +0100

    ahora se leen los datos desde teclado

--- AppAlumnoG.java
+++ AppAlumnoG.java
@@ -1,7 +1,6 @@
-//package Practica_II;
-
 import java.util.*;
 
+//Prueba de la clase AlumnoG
 public class AppAlumnoG
 {
     static Scanner teclado=new Scanner(System.in);
@@ -10,19 +9,28 @@ public class AppAlumnoG
     {
     
         String cad;
+	int edad;
 	int n[]=new int[3];
 	
         AlumnoG al=new AlumnoG(); 
-        al.setNombre("Jose");
-        al.setEdad(21);
-        al.setTelefono("777654321");
+	System.out.println("Introduce el nombre:");
+	cad=teclado.nextLine();
+        al.setNombre(cad);	
+	System.out.println("Introduce el telefono:");
+	cad=teclado.nextLine();
+        al.setTelefono(cad);
+	System.out.println("Introduce la edad:");
+	edad=teclado.nextInt();
+        al.setEdad(edad);
+
 	for (int i=0;i<n.length;i++)
 		{
 			n[i]=(int)(Math.random()*10);
 		}
 	al.setNotas(n);
         al.vis();
-              System.out.println("Los datos del alumno son");
+        System.out.println("Los datos del alumno son");
         System.out.println(al.toString());
     }
 }
+
